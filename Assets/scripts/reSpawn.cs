0ba012195f3ece89143e7d3ce9cbc3e8773bb3ef
CarolaOrbuch <101721194+CarolaOrbuch@users.noreply.github.com>
2022-06-10 10:40:51 -0300

    Avance clase 10/6
    Collision de barreras, respawn y recoleccion de monedas

---
+++ Assets/scripts/reSpawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class reSpawn : MonoBehaviour
+{
+    Vector3 spawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawn = new Vector3(16.98f, 0.5f, 0.3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter (Collider col)
+    {
+        if (col.gameObject.tag == "Barrera")
+        {
+            transform.position = spawn;
+        }
+    }
+}
